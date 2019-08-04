0a5d0f3e4f85f570ddd1693aa906ed6945ccb525
Varadharajan Mukundan <srinathsmn@stripe.com>
2019-08-03 22:11:23 -0700

    Resolve issues

--- cucumber-jvm-template-master/run_tests.sh
+++ cucumber-jvm-template-master/run_tests.sh
@@ -4,7 +4,7 @@ set -e
 
 
 # Waiting for selenoid to start
-docker-compose exec dockerize dockerize -wait tcp://selenoid:4444 -timeout 60s
+#docker-compose exec dockerize dockerize -wait tcp://selenoid:4444 -timeout 60s
 
 # Run tests
 docker-compose exec seleniumcode mvn test

63d2f4f26bee4e072de93ba744ad3e4815c2f9bc
Aaditya Seshadri <aseshadri@palo-it.com>
2019-08-04 13:24:19 +0800

    dockerpull included--

--- cucumber-jvm-template-master/run_tests.sh
+++ cucumber-jvm-template-master/run_tests.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+docker pull selenoid/vnc:chrome_75.0
+docker pull selenoid/video-recorder
+
 
 # Waiting for selenoid to start
 #docker-compose exec dockerize dockerize -wait tcp://selenoid:4444 -timeout 60s
